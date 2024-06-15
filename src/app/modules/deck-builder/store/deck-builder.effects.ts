eb3ffc6d6eb81cb96fb4c3855e0d296cb0df2ec7
Igor Milly <igor.milly@protonmail.com>
2024-06-15 14:13:24 +0200

    fix: misc imports

--- src/app/modules/deck-builder/store/deck-builder.effects.ts
+++ src/app/modules/deck-builder/store/deck-builder.effects.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { concatLatestFrom } from '@ngrx/operators';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs';
 import { CardRarity } from 'src/app/models/entity.models';
