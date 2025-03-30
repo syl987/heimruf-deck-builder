5280617ba6ee777bfcde1e99d2b81ba83f66c21d
Igor Milly <igor.milly@protonmail.com>
2025-03-30 16:51:31 +0200

    chore: kick unused pipe

--- src/app/models/regex.models.ts
+++
@@ -1,1 +0,0 @@
-export const XML_TAGS_REGEX = /(<([^>]+)>)/g;
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { EntityButtonComponent } from './components/entity-button/entity-button.component';
 import { EntityWrapperComponent } from './components/entity-wrapper/entity-wrapper.component';
-import { RemoveXmlTagsPipe } from './pipes/remove-xml-tags.pipe';
 import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 
 @NgModule({
-  imports: [EntityButtonComponent, EntityWrapperComponent, SafeHtmlPipe, RemoveXmlTagsPipe],
-  exports: [EntityButtonComponent, EntityWrapperComponent, SafeHtmlPipe, RemoveXmlTagsPipe],
+  imports: [EntityButtonComponent, EntityWrapperComponent, SafeHtmlPipe],
+  exports: [EntityButtonComponent, EntityWrapperComponent, SafeHtmlPipe],
 })
 export class SharedModule {}
