119b5c3ac554747afeb1ffe0e0649a772d776647
Igor Milly <igor.milly@protonmail.com>
2025-03-30 15:57:49 +0200

    chore: kick unused declarations

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { EntityButtonComponent } from './components/entity-button/entity-button.component';
 import { EntityWrapperComponent } from './components/entity-wrapper/entity-wrapper.component';
-import { ButtonSpinnerDirective } from './directives/button-spinner.directive';
 import { RemoveXmlTagsPipe } from './pipes/remove-xml-tags.pipe';
-import { ResponseErrorPipe } from './pipes/response-error.pipe';
 import { SafeHtmlPipe } from './pipes/safe-html.pipe';
-import { ValidationErrorPipe } from './pipes/validation-error.pipe';
-
-const EXPORTED_DECLARATIONS = [EntityButtonComponent, EntityWrapperComponent, ButtonSpinnerDirective, ResponseErrorPipe, ValidationErrorPipe, SafeHtmlPipe, RemoveXmlTagsPipe];
 
 @NgModule({
-  imports: [EXPORTED_DECLARATIONS],
-  exports: [EXPORTED_DECLARATIONS],
+  imports: [EntityButtonComponent, EntityWrapperComponent, SafeHtmlPipe, RemoveXmlTagsPipe],
+  exports: [EntityButtonComponent, EntityWrapperComponent, SafeHtmlPipe, RemoveXmlTagsPipe],
 })
 export class SharedModule {}
