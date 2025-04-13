dcd024d65bca10f45a4296eedcd9e8a80bb54d42
Igor Milly <igor.milly@protonmail.com>
2025-04-13 17:45:27 +0200

    refactor: align effects code

--- src/app/store/prerender/prerender.effects.ts
+++ src/app/store/prerender/prerender.effects.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import { createEffect } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, filter, switchMap } from 'rxjs';
+import { forkJoin, map, filter, switchMap } from 'rxjs';
 
 import { notEmpty } from 'src/app/functions/typeguard.functions';
 import { createCardComponents } from 'src/app/helpers/prerender.helpers';
 
 import { PrerenderActions } from './prerender.actions';
 import { selectPrefilteredCards } from '../prefilter/prefilter.selectors';
-import { from } from 'rxjs';
 
 @Injectable()
 export class PrerenderEffects {
   readonly initialized = createEffect(() => {
     return this.store.select(selectPrefilteredCards).pipe(
       filter(notEmpty),
-      switchMap(items => from(createCardComponents(items))),
-      map(items => PrerenderActions.initialized({ items })),
+      switchMap(items =>
+        forkJoin({
+          items: createCardComponents(items),
+        }).pipe(map(payload => PrerenderActions.initialized(payload))),
+      ),
     );
   });
 
