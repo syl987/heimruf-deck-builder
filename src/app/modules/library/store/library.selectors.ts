09832ac9ab8733560c91918224241fd6995acb1a
Igor Milly <igor.milly@protonmail.com>
2025-04-13 16:35:09 +0200

    feat: add library selector

--- src/app/modules/library/store/library.selectors.ts
+++ src/app/modules/library/store/library.selectors.ts
@@ -5,6 +5,7 @@ import { isCard } from 'src/app/helpers/entity.helpers';
 import { selectDataEntities } from 'src/app/store/data/data.selectors';
 import { selectCardPrefilterEntities } from 'src/app/store/prefilter/prefilter.selectors';
 import { selectRouterRouteParam } from 'src/app/store/router/router.selectors';
+import { selectCardPrerenderEntities } from 'src/app/store/prerender/prerender.selectors';
 
 export const selectLibraryCardIds = createSelector(selectCardPrefilterEntities, selectRouterRouteParam('cardClass'), (entities, param) => {
   return param ? (entities[param.toUpperCase()]?.cardIds ?? []) : [];
@@ -15,4 +16,8 @@ export const selectLibraryCards = createSelector(selectLibraryCardIds, selectDat
     .map(id => entities[id])
     .filter(notUndefined)
     .filter(isCard);
+});
+
+export const selectLibraryCardComponents = createSelector(selectLibraryCardIds, selectCardPrerenderEntities, (ids, entities) => {
+  return ids.map(id => entities[id]?.cardComponentRef).filter(notUndefined);
 });
