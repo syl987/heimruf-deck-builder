e4ef80d2316bf185d6d4a4677825f22492425e86
Igor Milly <igor.milly@protonmail.com>
2025-04-13 16:53:22 +0200

    refactor: adjust router selectors

--- src/app/store/router/router.selectors.ts
+++ src/app/store/router/router.selectors.ts
@@ -2,6 +2,9 @@ import { getRouterSelectors, createRouterSelector } from '@ngrx/router-store';
 
 export const selectRouterState = createRouterSelector();
 
-export const selectRouterUrl = getRouterSelectors(selectRouterState).selectUrl;
-export const selectRouterParam = getRouterSelectors(selectRouterState).selectRouteParam;
-export const selectRouterTitle = getRouterSelectors(selectRouterState).selectTitle;
+export const {
+  selectUrl: selectRouterUrl,
+  selectRouteParam: selectRouterRouteParam,
+  selectRouteParams: selectRouterRouteParams,
+  selectTitle: selectRouterTitle,
+} = getRouterSelectors(selectRouterState);
