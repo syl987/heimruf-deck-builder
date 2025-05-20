029932c91cb4657e561ed76a27ef5d294c61c5c0
syl987 <igor.milly@protonmail.com>
2025-05-20 14:16:48 +0200

    chore: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+/**
+ * @see https://github.com/stylelint/stylelint/blob/main/docs/user-guide/configure.md
+ *
+ * @type {import('stylelint').Config}
+ */
+const config = {
+  extends: [
+    'stylelint-config-recommended-scss', // also extends: recommended
+    'stylelint-config-clean-order',
+    'stylelint-config-prettier-scss',
+  ],
+  ignoreFiles: ['./src/styles/bootstrap/**.*'],
+  rules: {
+    'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['ng-deep'] }], // recommended override: support angular elements
+  },
+};
+
+module.exports = config;
