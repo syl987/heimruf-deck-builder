b847d6aa409e0b08d85bda39c60ced02cf5ac63b
syl987 <igor.milly@protonmail.com>
2025-12-05 14:18:11 +0100

    feat(data): reduce neutral cards

--- src/app/store/data/data.effects.ts
+++ src/app/store/data/data.effects.ts
@@ -4,7 +4,19 @@ import { Action } from '@ngrx/store';
 import { catchError, concatMap, map, of } from 'rxjs';
 
 import { DataActions } from './data.actions';
+import { Entity } from '../../models/entity.models';
 import { DataService } from '../../services/data.service';
+
+const NEUTRAL_CARD_REDUCTION_FACTOR = 10; // reduce number of neutral cards with cost < 8 by this factor
+
+/**
+ * Filter to reduce number of neutral cards with the specified cost.
+ *
+ * @param [factor=NEUTRAL_CARD_REDUCTION_FACTOR] reduction rate
+ */
+function reduceNeutralCards(factor = NEUTRAL_CARD_REDUCTION_FACTOR) {
+  return map<Entity[], Entity[]>(cardData => cardData.filter((c, i) => (c.type !== 'HERO' && c.cardClass === 'NEUTRAL' && c.cost < 8 ? i % factor === 0 : true)));
+}
 
 @Injectable()
 export class DataEffects implements OnInitEffects {
@@ -13,6 +25,7 @@ export class DataEffects implements OnInitEffects {
       ofType(DataActions.load),
       concatMap(_ =>
         this.dataService.loadCardData().pipe(
+          reduceNeutralCards(), // workaround: reduce rendered data for performance reasons
           map(cardData => DataActions.loadSUCCESS({ cardData })),
           catchError((err: unknown) => of(DataActions.loadERROR())),
         ),
