b5dd333f3f5dcc58315ec9d7b5a62060c6ef1a5a
syl987 <igor.milly@protonmail.com>
2025-12-05 14:23:46 +0100

    refactor: migrate to inject method

--- src/app/modules/shared/pipes/safe-html.pipe.ts
+++ src/app/modules/shared/pipes/safe-html.pipe.ts
@@ -1,11 +1,11 @@
-import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
+import { Pipe, PipeTransform, SecurityContext, inject } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
   name: 'safeHtml',
 })
 export class SafeHtmlPipe implements PipeTransform {
-  constructor(private readonly sanitizer: DomSanitizer) {}
+  private readonly sanitizer = inject(DomSanitizer);
 
   transform(value: string | null | undefined): SafeHtml {
     if (value === null || value === undefined || typeof value !== 'string') {
